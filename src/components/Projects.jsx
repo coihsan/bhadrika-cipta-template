87a911c5adbdaf3dc13065ccfe6d8651cef5ae0f
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-26 19:59:01 +0700

    1 file modified, 1 added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,15 @@ function Projects(){
     <div className="section m-auto">
       <div className="container">
       <h1 className="text-5xl mb-8 font-bold">Projects</h1>
+      <div className="overflow-scroll">
+        <div className="">
+          <div className="">
+            <span>01</span>
+            <h1>Custom Landing Page</h1>
+            <a href="#"></a>
+          </div>
+        </div>
+      </div>
       <div className="line-separator-hero">
           <span></span>
         </div>
