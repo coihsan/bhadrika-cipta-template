2c20bf06a91a08129b2b1c5fe54d92553306e003
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-26 19:26:35 +0700

    2 files modified

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,12 +10,12 @@ function Skills(){
       <h1 className="text-5xl mb-8 font-bold">Skills</h1>
       {/* content */}
       <div className="flex items-center gap-3">
-          <div className="bg-gradient-to-l from-violet-500 to-fuchsia-500 rounded-md p-px">
-            <div className="p-5 bg-neutral-900 rounded-md">
+          <div className="bg-gradient-to-t from-transparent via-zinc-500 to-zinc-900 rounded-md p-px">
+            <div className="p-5 bg-neutral-950 rounded-md">
               {/* skills header */}
               <div className="mb-6" >
                 <img className="w-14" src={smile} alt=""/>
-                <h3 className="text-3xl font-bold">Web / SEO</h3>
+                <h3 className="text-3xl font-bold">Digital {'{Marketing}'}</h3>
                 <p>I am a web designer with experience in creating landing pages using HTML, CSS, and JavaScript.</p>
               </div>
               {/* skills header */}
@@ -27,7 +27,7 @@ function Skills(){
                   <span className="w-2/5 bg-blue-500 h-2.5"></span>
                   <span className="w-2/5 bg-lime-500 h-2.5"></span>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mb-5">
                   <span className="relative font-bold text-sm">HTML</span>
                   <span className="relative font-bold text-sm">CSS</span>
                   <span className="relative font-bold text-sm">JS</span>
@@ -37,13 +37,13 @@ function Skills(){
               <div>
                 <h5 className="font-bold">Skills &amp; Tool's</h5>
                 <ul className="flex items-center flex-wrap mt-3 gap-2">
-                  <li className="py-1 px-3 bg-zinc-700 rounded">On-Page SEO</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Off-Page SEO</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Landing Page Creation</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Yoast</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">SEMRush</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Search Console</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Analytics</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Google Ads</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Meta Ads</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Social Media Content</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Event</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">E-Commerce / Marketplace</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Copywriting</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">PPC</li>
                   <img src={dot} alt=""/>
                 </ul>
                 <div className="mt-4 flex flex-wrap">
@@ -55,8 +55,8 @@ function Skills(){
               </div>
             </div>
           </div>
-          <div className="bg-gradient-to-l from-violet-500 to-fuchsia-500 rounded-md p-px">
-            <div className="p-5 bg-neutral-900 rounded-md">
+          <div className="bg-gradient-to-t from-transparent via-zinc-500 to-zinc-900 rounded-md p-px">
+            <div className="p-5 bg-neutral-950 rounded-md">
               {/* skills header */}
               <div className="mb-6" >
                 <img className="w-14" src={smile} alt=""/>
@@ -72,7 +72,7 @@ function Skills(){
                   <span className="w-2/5 bg-blue-500 h-2.5"></span>
                   <span className="w-2/5 bg-lime-500 h-2.5"></span>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mb-5">
                   <span className="relative font-bold text-sm">HTML</span>
                   <span className="relative font-bold text-sm">CSS</span>
                   <span className="relative font-bold text-sm">JS</span>
@@ -82,13 +82,13 @@ function Skills(){
               <div>
                 <h5 className="font-bold">Skills &amp; Tool's</h5>
                 <ul className="flex items-center flex-wrap mt-3 gap-2">
-                  <li className="py-1 px-3 bg-zinc-700 rounded">On-Page SEO</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Off-Page SEO</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Landing Page Creation</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Yoast</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">SEMRush</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Search Console</li>
-                  <li className="py-1 px-3 bg-zinc-700 rounded">Analytics</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">On-Page SEO</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Off-Page SEO</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Landing Page Creation</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Yoast</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">SEMRush</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Search Console</li>
+                  <li className="py-1 px-3 bg-zinc-800 rounded">Analytics</li>
                   <img src={dot} alt=""/>
                 </ul>
                 <div className="mt-4 flex flex-wrap">
