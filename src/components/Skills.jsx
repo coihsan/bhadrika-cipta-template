1cc89b2d6e380ca3aed9341dfe28c224d3372ebc
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-26 18:41:59 +0700

    1 file modified

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,10 +10,28 @@ function Skills(){
       <div className="flex items-center">
           <div className="bg-gradient-to-l from-violet-500 to-fuchsia-500 rounded-md p-px">
             <div className="p-5 bg-neutral-900 rounded-md">
-              <div className="skills-header" >
+              {/* skills header */}
+              <div className="mb-2" >
                 <img className="mb-4 w-14" src={smile} alt=""/>
-                <h3 className="">Web Design</h3>
+                <h3 className="text-2xl">Web / SEO</h3>
+                <p>I am a web designer with experience in creating landing pages using HTML, CSS, and JavaScript.</p>
               </div>
+              {/* skills header */}
+              {/* Progress */}
+              <div className="grid gap-3">
+                <h5>Most Used Languages</h5>
+                <div className="flex items-center relative w-full overflow-hidden h-2.5 rounded-md">
+                  <span className="w-2/5 bg-orange-500 h-2.5"></span>
+                  <span className="w-2/5 bg-blue-500 h-2.5"></span>
+                  <span className="w-2/5 bg-lime-500 h-2.5"></span>
+                </div>
+                <div>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JS</span>
+                </div>
+              </div>
+              {/* Progress */}
             </div>
           </div>
         </div>
