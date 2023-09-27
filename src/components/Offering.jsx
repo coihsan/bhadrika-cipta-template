fac2dfd2b61299995b3d6e84af52ae884bf54c44
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-27 15:47:51 +0700

    2 files modified

--- src/components/Offering.jsx
+++ src/components/Offering.jsx
@@ -10,34 +10,34 @@ function Offering(){
       <div className="container">
         <h1 className="text-5xl mb-8 font-bold">Offering</h1>
         <div className="overflow-hidden w-full">
-        <div className="overflow-scroll scroll-smooth">
-        <div className="grid grid-cols-2 gap-5 w-full">
-          <a href="/landing-pages-services.jsx" className="relative w-full border border-zinc-700 bg-gradient-to-tl from-zinc-900 to-zinc-950 p-10 rounded-md hover:border-zinc-200 transition-colors">
-            <div className="p-px bg-gradient-to-br from-amber-500 via-fuchsia-500 to-indigo-800 rounded-md mb-4 w-max">
+        <div className="overflow-scroll scroll-smooth card-offering-wrapper">
+        <div className="flex gap-5 w-full py-10">
+          <a href="/landing-pages-services.jsx" className="shrink-0 basis-96 relative w-full border border-zinc-700 bg-gradient-to-tl from-zinc-900 to-zinc-950 p-10 rounded-md hover:border-zinc-200 transition-colors">
+            <div className="p-0.5 bg-gradient-to-br from-amber-500 via-fuchsia-500 to-indigo-800 rounded-md mb-4 w-max">
             <img className="text-2xl py-3 px-3 flex font-bold bg-zinc-950 rounded-md" src={laptop} alt=""/>
             </div>
             <h1 className="text-3xl font-bold my-2">Custom Landing Page</h1>
             <p>Create effective landing pages for your marketing campaigns, product launches, affiliate programs, and support A/B Testing.</p>
             <a className="absolute bottom-5 right-5" href="#"><img src={arrow} alt=""/></a>
           </a>
-          <a href="#" className="relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors ">
-            <div className="p-px bg-gradient-to-br from-lime-500 via-orange-500 to-blue-800 rounded-md mb-4 w-max">
+          <a href="#" className="shrink-0 basis-96 relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors ">
+            <div className="p-0.5 bg-gradient-to-br from-lime-500 via-orange-500 to-blue-800 rounded-md mb-4 w-max">
             <img className="text-2xl py-3 px-3 flex font-bold bg-zinc-950 rounded-md" src={ads} alt=""/>
             </div>
             <h1 className="text-3xl font-bold my-2">Google &amp; Social Media Ads</h1>
             <p>Create effective landing pages for your marketing campaigns, product launches, affiliate programs, and support A/B Testing.</p>
             <a className="absolute bottom-5 right-5" href="#"><img src={arrow} alt=""/></a>
           </a>
-          <a href="#" className="relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors">
-            <div className="p-px bg-gradient-to-br from-amber-500 via-fuchsia-500 to-indigo-800 rounded-md mb-4 w-max">
+          <a href="#" className="shrink-0 basis-96 relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors">
+            <div className="p-0.5 bg-gradient-to-br from-sky-500 via-fuchsia-500 to-indigo-800 rounded-md mb-4 w-max">
             <img className="text-2xl py-3 px-3 flex font-bold bg-zinc-950 rounded-md" src={design} alt=""/>
             </div>
             <h1 className="text-3xl font-bold my-2">Social Media Content</h1>
             <p>Create effective landing pages for your marketing campaigns, product launches, affiliate programs, and support A/B Testing.</p>
             <a className="absolute bottom-5 right-5" href="#"><img src={arrow} alt=""/></a>
           </a>
-          <a href="" className="relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors">
-            <div className="p-px bg-gradient-to-br from-amber-500 via-fuchsia-500 to-indigo-800 rounded-md mb-4 w-max">
+          <a href="" className="shrink-0 basis-96 relative w-full border border-zinc-700 p-10 rounded-md bg-gradient-to-tl from-zinc-900 to-zinc-950 hover:border-zinc-200 transition-colors">
+            <div className="p-0.5 bg-gradient-to-br from-lime-500 via-rose-500 to-sky-800 rounded-md mb-4 w-max">
             <img className="text-2xl py-3 px-3 flex font-bold bg-zinc-950 rounded-md" src={search} alt=""/>
             </div>
             <h1 className="text-3xl font-bold my-2">Search Engine Optimization</h1>
