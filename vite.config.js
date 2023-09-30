78b31a7cbcf37731e2ce2fe7213bae2dae24fc3e
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-30 23:09:04 +0700

    1 file modified, 1 added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } = require('vite');
+import { vitePluginGhPages } = require('vite-plugin-gh-pages');
 
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+module.exports = defineConfig({
+  plugins: [react(),
+    vitePluginGhPages()],
+});
