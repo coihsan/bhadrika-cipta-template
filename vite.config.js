2c5c7890fa5066034505b8f8fda8493c0400c6e8
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-30 22:11:01 +0700

    4 files modified, 5 added, 7 deleted

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: '/',
+    
   }
 
   if (command !== 'serve') {
