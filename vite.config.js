43b355a40d6aff290e046fd3fb8f0a4a73b08163
coihsan <110887888+coihsan@users.noreply.github.com>
2023-09-30 22:39:14 +0700

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages';
+
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-    
-  }
-
-  if (command !== 'serve') {
-    config.base = '/vite-first/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
