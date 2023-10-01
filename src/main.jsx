84c60dfedcd9acb637e6d667aa11a831b06c5981
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-01 10:42:28 +0700

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from 'react-dom/client'
-
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
