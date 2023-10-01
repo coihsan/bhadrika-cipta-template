6a60b4a775652f6f379701db3969ce079e7e07f4
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2023-10-01 23:38:15 +0700

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import telegram from '../assets/telegram.svg';
 
 function Hero(){
   return(
-    <div className="hero-section section flex items-center w-full">
+    <div className="hero-section section flex items-center w-full ">
       <div className="hero-container container flex flex-col justify-center">
         <p className="Hello max-[470px]:text-4xl max-[470px]:text-3xl">Hi there, I'm <span className="text-color">Ihsan </span>👋🏻</p>
         <p className="Hello max-[470px]:text-4xl max-[470px]:text-3xl">Digital Marketing <span className="text-color">&amp; {'{Front-End Dev}'}</span></p>
