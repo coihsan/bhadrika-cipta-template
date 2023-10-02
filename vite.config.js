254d67a376cff648c3ee0c5f2c495455dc22563e
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-02 20:14:38 +0700

    5 files modified, 6 added, 4 deleted

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactPages from 'vite-plugin-react-pages';
+// import { resolve } from 'path'
 
+// const root = resolve(__dirname, 'src')
+// const outDir = resolve(__dirname, 'src')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactPages(), ],
+  plugins: [react()],
   base: 'https://coihsan.github.io/vite-first/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        services: './src/pages/Services.jsx',
+      },
+      output: {
+        css: './dist/main.css',
+        js: './dist/main.js',
+      },
+    },
+  },
 })
