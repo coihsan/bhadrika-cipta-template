e0de437b49fa3d8a228680dcd2441ac55488560a
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-03 11:03:46 +0700

    1 file modified

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,21 +11,21 @@ function Projects(){
           <img className="w-9" src={sparkless} alt=""/>
           <h1 className="text-5xl font-bold max-[470px]:text-4xl">Resources</h1>
         </div>
-          <a className="hover:underline" href="#">Index</a>
+          <a className="hover:underline px-4 py-1 text-[14px] bg-zinc-800 rounded-full" href="#">Index</a>
         </div>
         <div className="grid grid-cols-2 gap-3 max-[600px]:grid-cols-1">
           {/* content 1 */}
-          <div className="relative rounded-md overflow-hidden">
+          <div className="relative overflow-hidden">
           <img className="hover:scale-[1.1] delay-0 transition-all duration-[1000ms] ease-in origin-top" src={'https://coihsan.github.io/profile/assets/jhondoe-hero.webp'} alt=""/>
-            <div className="absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-black/30 backdrop-opacity-10 backdrop-invert rounded-s">
+            <div className="absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-white/10 backdrop-opacity-10 backdrop-invert">
               <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={github} alt=""/>Get Code</a>
               <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={preview} alt=""/>Live Demo</a>
             </div>
           </div>
           {/* content 2 */}
-          <div className="relative rounded-md overflow-hidden">
+          <div className="relative overflow-hidden">
           <img className="hover:scale-[1.1] delay-0 transition-all duration-[1000ms] ease-in origin-top" src={'https://coihsan.github.io/profile/assets/Mack-Smith-Resume-2023.webp'} alt=""/>
-          <div className=" absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-black/30 backdrop-opacity-10 backdrop-invert rounded-s">
+          <div className=" absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-white/10 backdrop-opacity-10 backdrop-invert">
               <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={github} alt=""/>Get Code</a>
               <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={preview} alt=""/>Live Demo</a>
             </div>
