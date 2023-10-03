1a2519cea7a4ab2ff8c1ff3a8a1907253b3aa5c9
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-03 13:51:25 +0700

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Aboutme from "./components/Aboutme";
@@ -11,23 +10,23 @@ import Contact from "./components/Contact";
 
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  // const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/App">
+      <Routes>
+      <Route path="/App"/>
       <Navbar/>
       <Hero />
         <Aboutme />
-        
         <div className="h-[60%] w-full bg-gradient-to-t from-zinc-950 via-[#4e6eff0e] to-zinc-950 ">
           <Skills/>
           <Offering/>
         </div>
-        
         <Projects/>
         <Contact/>
-      
+        </Routes>
     </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import App from './App.jsx';
 import './index.css';
 import Root from "./routes/root";
