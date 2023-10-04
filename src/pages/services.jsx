7fa1a889f3b4a25ce8240880a2c2332523933064
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-04 20:02:38 +0700

    2 files modified

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-// import Navbar from "../components/Navbar";
-
-const Services = () =>{
+import { useState } from 'react'
+import preview from '../assets/preview.svg';
+import github from '../assets/github.svg';
+import sparkless from '../assets/sparkless.svg';
+function Services (){
   return (
-    <section>
-      {/* <Navbar/> */}
-      <div className="section max-[600px]:w-full">
+    <div className="section max-[600px]:w-full">
       <div className="container">
+        <div className="mb-8 flex items-end justify-between">
+          <div className="flex items-center gap-3">
+          <img className="w-9" src={sparkless} alt=""/>
+          <h1 className="text-5xl font-bold max-[470px]:text-4xl">Resources</h1>
+        </div>
+          <a className="hover:underline px-4 py-1 text-[14px] bg-zinc-800 rounded-full" href="#">Index</a>
+        </div>
         <div className="grid grid-cols-2 gap-3 max-[600px]:grid-cols-1">
           {/* content 1 */}
           <div className="relative overflow-hidden">
@@ -26,8 +32,8 @@ const Services = () =>{
           </div>
         </div>
       </div>
+      <div className="mb-separator w-full h-px bg-zinc-800"></div>
     </div>
-    </section>
   )
 }
 
