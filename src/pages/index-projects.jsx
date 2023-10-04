f36354d98555a177f77eaee56f86f787c681482b
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-04 20:51:10 +0700

    1 file deleted

---
+++ src/pages/index-projects.jsx
@@ -0,0 +1,22 @@
+const IndexProjects = () =>{
+  return (
+    <div className="grid grid-cols-2 gap-3 max-[600px]:grid-cols-1">
+          {/* content 1 */}
+          <div className="relative overflow-hidden">
+          <img className="hover:scale-[1.1] delay-0 transition-all duration-[1000ms] ease-in origin-top" src={'https://coihsan.github.io/profile/assets/jhondoe-hero.webp'} alt=""/>
+            <div className="absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-white/10 backdrop-opacity-10 backdrop-invert">
+              <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={github} alt=""/>Get Code</a>
+              <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={preview} alt=""/>Live Demo</a>
+            </div>
+          </div>
+          {/* content 2 */}
+          <div className="relative overflow-hidden">
+          <img className="hover:scale-[1.1] delay-0 transition-all duration-[1000ms] ease-in origin-top" src={'https://coihsan.github.io/profile/assets/Mack-Smith-Resume-2023.webp'} alt=""/>
+          <div className=" absolute bottom-0 right-0 flex items-center backdrop-blur-md bg-white/10 backdrop-opacity-10 backdrop-invert">
+              <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={github} alt=""/>Get Code</a>
+              <a className="text-sm flex items-center gap-2 hover:bg-zinc-900 px-3 py-1" href="#"><img className="w-4" src={preview} alt=""/>Live Demo</a>
+            </div>
+          </div>
+        </div>
+  )
+}
