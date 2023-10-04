165e883208d47df1eef36e66c1be7d7a29470a77
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2023-10-04 14:00:40 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Aboutme from "./components/Aboutme";
@@ -10,18 +11,20 @@ import Contact from "./components/Contact";
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <section>
-      <Navbar/>
-      <Hero />
-      <Aboutme />
-      <Skills/>
-      <Offering/>
-      <Projects/>
-      <Contact/>
-    </section>
+    <Router basename="/App" >
+      <Routes>
+      <Route path="/" element={<App />} exact>
+        <Navbar/>
+        <Hero />
+        <Aboutme />
+        <Skills/>
+        <Offering/>
+        <Projects/>
+        <Contact/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
