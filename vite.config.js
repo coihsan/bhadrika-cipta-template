173811ea7f1fcbd34cfcf2cf1eea6a54e364b4d9
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2023-10-05 10:44:01 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   // base: 'https://coihsan.github.io/vite-first/',
 })
